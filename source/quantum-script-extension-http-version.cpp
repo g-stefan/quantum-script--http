52f5f05f9a776231c5325b3435374c1ba089d323
Grigore Stefan <g_stefan@yahoo.com>
2021-11-26 03:22:14 +0200

    Update

--- source/quantum-script-extension-http-version.cpp
+++ source/quantum-script-extension-http-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace HTTP {
 				namespace Version {
 
-					static const char *version_ = "2.2.0";
+					static const char *version_ = "2.4.0";
 					static const char *build_ = "21";
-					static const char *versionWithBuild_ = "2.2.0.21";
-					static const char *datetime_ = "2021-11-15 16:51:51";
+					static const char *versionWithBuild_ = "2.4.0.21";
+					static const char *datetime_ = "2021-11-22 17:08:00";
 
 					const char *version() {
 						return version_;
