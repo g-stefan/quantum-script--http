d70d4bbc37ef68ee4d54080b2df7d5132e46eceb
Grigore Stefan <g_stefan@yahoo.com>
2024-03-28 12:10:52 +0200

    Update

--- source/XYO/QuantumScript.Extension/HTTP/Library.cpp
+++ source/XYO/QuantumScript.Extension/HTTP/Library.cpp
@@ -31,7 +31,7 @@ namespace XYO::QuantumScript::Extension::HTTP {
 
 };
 
-#ifdef XYO_COMPILE_DYNAMIC_LIBRARY
+#ifdef XYO_PLATFORM_COMPILE_DYNAMIC_LIBRARY
 extern "C" XYO_QUANTUMSCRIPT_EXTENSION_HTTP_EXPORT void quantumScriptExtension(XYO::QuantumScript::Executive *executive, void *extensionId) {
 	XYO::QuantumScript::Extension::HTTP::initExecutive(executive, extensionId);
 };
